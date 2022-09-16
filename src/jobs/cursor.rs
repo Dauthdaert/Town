71e7cbac35923e3c4c8f787b3b1fe3352c66088a
Wayan-Gwie Lapointe <wayangwie@gmail.com>
2022-09-15 22:10:18 -0400

    Add selection ui to provide player with visualization of current selection

--- src/jobs/cursor.rs
+++ src/jobs/cursor.rs
@@ -1,9 +1,12 @@
-use bevy::{math::Vec3Swizzles, prelude::*};
+use bevy::{math::Vec3Swizzles, prelude::*, sprite::Anchor};
 use bevy_mouse_tracking_plugin::MousePosWorld;
 
-use crate::map_gen::map::{tile_xy_world_xy, world_xy_tile_xy};
+use crate::map_gen::{
+    map::{tile_xy_world_xy, world_xy_tile_xy},
+    TILE_SIZE,
+};
 
-use super::JobCreationMenuAssets;
+use super::{job_creation::SelectionStart, JobCreationMenuAssets};
 
 #[derive(Component, Clone, Copy, Debug)]
 pub struct JobCreationMenuCursor;
@@ -16,6 +19,11 @@ pub fn setup_job_creation_menu_cursor(mut commands: Commands, job_creation_menu_
                 translation: Vec2::ZERO.extend(2.),
                 ..default()
             },
+            sprite: Sprite {
+                color: Color::BLUE,
+                anchor: Anchor::TopLeft,
+                ..default()
+            },
             ..default()
         })
         .insert_bundle((Name::from("Job Creation Menu Cursor"), JobCreationMenuCursor));
@@ -23,6 +31,7 @@ pub fn setup_job_creation_menu_cursor(mut commands: Commands, job_creation_menu_
 
 pub fn job_creation_menu_cursor_follow_mouse(
     mut cursor: Query<&mut Transform, With<JobCreationMenuCursor>>,
+    selection: Option<Res<SelectionStart>>,
     mouse_pos: Res<MousePosWorld>,
 ) {
     let mut cursor_transform = cursor.single_mut();
@@ -30,5 +39,37 @@ pub fn job_creation_menu_cursor_follow_mouse(
     let world_tile = world_xy_tile_xy(mouse_pos.xy());
     let world_pos = tile_xy_world_xy(world_tile.x, world_tile.y);
 
-    cursor_transform.translation = world_pos.extend(10.0);
+    let (pos, size) = if let Some(selection_start) = selection {
+        let size_x = selection_start.x.abs_diff(world_tile.x) as f32 + 1.;
+        let size_y = selection_start.y.abs_diff(world_tile.y) as f32 + 1.;
+        let size = Vec2::new(size_x, size_y);
+
+        if world_tile.x >= selection_start.x && world_tile.y <= selection_start.y {
+            let selection_pos = tile_xy_world_xy(selection_start.x, selection_start.y);
+
+            (shift_to_anchor(selection_pos), size.extend(1.0))
+        } else if world_tile.x <= selection_start.x && world_tile.y <= selection_start.y {
+            let selection_pos = tile_xy_world_xy(selection_start.x, selection_start.y);
+            let pos = shift_to_anchor(Vec2::new(world_pos.x, selection_pos.y));
+
+            (pos, size.extend(1.0))
+        } else if world_tile.x <= selection_start.x && world_tile.y >= selection_start.y {
+            (shift_to_anchor(world_pos), size.extend(1.0))
+        } else {
+            //if world_tile.x >= selection_start.x && world_tile.y >= selection_start.y
+            let selection_pos = tile_xy_world_xy(selection_start.x, selection_start.y);
+            let pos = shift_to_anchor(Vec2::new(selection_pos.x, world_pos.y));
+
+            (pos, size.extend(1.0))
+        }
+    } else {
+        (shift_to_anchor(world_pos), Vec3::splat(1.0))
+    };
+
+    cursor_transform.scale = size;
+    cursor_transform.translation = pos.extend(10.0);
+}
+
+fn shift_to_anchor(pos: Vec2) -> Vec2 {
+    pos - Vec2::new(TILE_SIZE.x / 2.0, -TILE_SIZE.y / 2.0)
 }
